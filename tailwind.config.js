89fd8c6556549c0584826480e06d0af292506527
Arash Soltani <soltaniworld@gmail.com>
2024-03-20 01:42:44 -0400

    final draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ module.exports = {
         "myFont" : ['"Kumbh Sans"', 'sans-serif']
       },
       spacing: {
-        '88': '22rem'
+        '18':'4.5rem',
+        '19':'4.75rem',
+        '22':'5.5rem',
+        '88': '22rem',
+        '230': '57.5rem', // 920px,
+        '127': '31.75rem', // 508px
+        '132':'33rem'
+        // 966px
+      },
+      colors:{
+        "top-purple": "#B068E9",
+        "bottom-purple": "#6463E7",
+        "hover-orange": "#F47B56"
       }
     },
   },
